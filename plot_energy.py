b423c3e1fa4026436c0e2eb2766fd5f3fe15f9a6
Abraham Francis <abfr049@gmail.com>
2020-02-22 19:34:35 +0530

    add multiple graph generator

--- plot_energy.py
+++ plot_energy.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plot
 
 fig, ax = plot.subplots()
 
-ax.set_xlim(0, 100)
-Y_max=50
+ax.set_xlim(0, 200)
+Y_max=75
 ax.set_ylim(0, Y_max)
-ax.set_xlabel('Time')
+ax.set_xlabel('Time (ms)')
 ax.set_ylabel('Energy (mJ)')
 ax.grid(True)
 
@@ -31,10 +31,11 @@ for pts in plot_dat:
 	utl.append(pts[0])
 	ltf.append(pts[1])
 	ltf_us.append(pts[2])
+	# ax.plot([pts[0], pts[0]], [pts[1], pts[2]], '-', color="blue", linewidth=0.01)
 
-ax.plot(utl, ltf, 'ro-.')
-ax.plot(utl, ltf_us, 'bo-.')
+ltf_line = ax.plot(utl, ltf, '.', markersize=0.5, color="red", label="LTF")
+ltf_us_line = ax.plot(utl, ltf_us, '.', markersize=0.5, color="green", label="LTF US")
 
 # fig.suptitle(f'Energy: {round(energy, 4)} mJ     Finish Time: {round(max(max1, max2), 4)} ms', fontsize=16)
-
+fig.legend(markerscale=32.0, fontsize=10)
 plot.show()
