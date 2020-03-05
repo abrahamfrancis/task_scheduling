26355d424e81a372725c86c3a71649380de8a610
Abraham Francis <abfr049@gmail.com>
2020-03-05 16:52:39 +0530

    add comments on working

--- ltf_schedule.cpp
+++ ltf_schedule.cpp
@@ -25,6 +25,11 @@ schedule ltf_schedule(const std::vector<task> &tasks, const adj_list &graph)
 	schedule ltf;
 	std::vector<int> parent_tasks = parent_task_count(graph);
 	ready_list ready_tasks;
+
+	/*
+		Fill ready tasks with orphan tasks -
+		ones with no dependencies
+	 */
 	for (unsigned int i = 0; i < parent_tasks.size(); ++i) {
 		if (parent_tasks[i] == 0) {
 			ready_tasks.push(tasks[i]);
@@ -36,7 +41,9 @@ schedule ltf_schedule(const std::vector<task> &tasks, const adj_list &graph)
 		task ready = ready_tasks.top();
 		ready_tasks.pop();
 		double time;
+
 		if ((ltf.lpt() <= ltf.hpt()) && (ltf.lpt() + ready.lp_size() <= MAX_DUR)) {
+		// LP Slot is Free and current task will not exceed MAX_DUR
 			ltf.add_LP_slot(ready);
 			if (last_lp != -1) clear_tasks(ready_tasks, tasks, graph[last_lp], parent_tasks);
 			last_lp = ready.get_id();
@@ -47,6 +54,7 @@ schedule ltf_schedule(const std::vector<task> &tasks, const adj_list &graph)
 				last_hp = -1;
 			}
 		} else {
+		// Add task to HP Slot
 			ltf.add_HP_slot(ready);
 			if (last_hp != -1) clear_tasks(ready_tasks, tasks, graph[last_hp], parent_tasks);
 			last_hp = ready.get_id();
--- parent_task_count.cpp
+++ parent_task_count.cpp
@@ -1,5 +1,9 @@
 #include "schedule.hpp"
 
+/*
+	Get Number of task dependencies (or parents)
+	for each task
+ */
 std::vector<int> parent_task_count(const adj_list &graph) {
 	std::vector<int> parent_tasks(graph.size(), 0);
 	for (auto &children : graph) {
